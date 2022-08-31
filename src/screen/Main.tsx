538682e7c662c07c2c6eeb8fa5e06378f1005ac3
nacora <bdg407@gmail.com>
2022-08-31 23:00:12 +0900

    header 수정

--- src/screen/Main.tsx
+++ src/screen/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
-import {Header} from '../components/Header/Header';
+import {Header} from '../components/Header/Index';
 import {Styles} from './Styles';
 
 export const Main = () => {
